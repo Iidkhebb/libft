6b7840178751674a0dec1e5fda017675bfddc8b9
Ilyasse Idkhebbach <iidkhebb@e2r9p7.1337.ma>
2021-11-03 12:48:18 +0100

    5nd

--- all/ft_strtrim.c
+++ all/ft_strtrim.c
@@ -1,5 +1,24 @@
 #include "libft.h"
-char *ft_strtrim(char const *s1, char const *set)
+char	*ft_strtrim(char const *s1, char const *set)
 {
-    
+	size_t	i;
+    char * out;
+
+	if (!(s1) || !(set))
+		return (0);
+	while (*s1 && ft_strchr(set, *s1))
+		s1++;
+	i = ft_strlen(s1);
+	while (i && ft_strchr(set, s1[i - 1]))
+		i--;
+    out = ft_substr(s1, 0, i);
+	return (out);
 }
+/*
+int main ()
+{
+    char const str[] = "abxyrdahja-ba";
+    char const set[] = "ab";
+    printf("%s", ft_strtrim(str, set));
+}
+*/
