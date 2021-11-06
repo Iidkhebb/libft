c7d57418de3bec036f751c1eae60c37ac31185aa
Ilyasse Idkhebbach <iidkhebb@e1r8p13.1337.ma>
2021-11-06 12:15:30 +0100

    all good

--- all/ft_lstmap.c
+++ all/ft_lstmap.c
@@ -1,5 +1,24 @@
 #include "libft.h"
 t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
+    t_list *new_lst;
     
+    if(!(lst))
+        return 0;
+    new_lst = NULL;
+    while (lst->next)
+    {
+        new_lst = ft_lstnew(f(lst->content));
+        if(!(new_lst))
+        {
+            del(lst);
+            free(lst);
+            return (0);
+        }
+        new_lst = ft_lstlast(new_lst);
+	    new_lst->next = new_lst;
+        
+        lst = lst->next;
+    }
+    return(new_lst);
 }
