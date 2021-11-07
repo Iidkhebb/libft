4733bacd2cd8acf5604cbf49becb6428979448b8
Ilyasse Idkhebbach <93012484+Iidkhebb@users.noreply.github.com>
2021-11-07 14:02:08 +0100

    Update ft_strjoin.c

--- all/ft_strjoin.c
+++ all/ft_strjoin.c
@@ -2,10 +2,8 @@
 
 char *ft_strjoin(char const *s1, char const *s2)
 {
-    int i;
     char *s;
 
-    i = 0;
     s = (char *)malloc(ft_strlen(s1)+ft_strlen(s2) + 1);
     if ((!(s1) && !(s2)) || !(s))
         return (0);
